6dbb5644a079433f95fb35ad54ca4a92fb9cad5c
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-11-23 17:40:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,17 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "&";
+var prefix = "*";
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`*help |the bot by Ayman ALmonster`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
+});
 
 client.on('message', message => {
     var prefix = "&";
