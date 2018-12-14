9d8a43bd5edf46844ad28eec2b69dbccb6dd7a7a
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-12-15 01:26:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -411,11 +411,6 @@ client.on('message', message => {
             message.guild.members.forEach(m => {
        if(!message.member.hasPermission('ADMINISTRATOR')) return;
                 var bc = new Discord.RichEmbed()
-                .addField('» السيرفر :', `${message.guild.name}`)
-                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-                .addField(' » الرسالة : ', args)
-                .setColor('RANDOM')
-                // m.send(`[${m}]`);
                 m.send(`${m}`,{embed: bc});
             });
         }
