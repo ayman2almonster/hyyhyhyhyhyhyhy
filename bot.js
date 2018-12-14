a4e294432185074488e0b4485feb62de301b5849
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-12-15 01:50:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -411,7 +411,6 @@ client.on('message', message => {
             message.guild.members.forEach(m => {
        if(!message.member.hasPermission('ADMINISTRATOR')) return;
                 var bc = new Discord.RichEmbed()
-
             });
         }
         } else {
