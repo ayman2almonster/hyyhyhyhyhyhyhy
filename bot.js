9d20a66189d28bc61fe3972617b4e0b54dbcfa88
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-12-15 01:41:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -404,9 +404,8 @@ client.on('message', message => {
        let embed = new Discord.RichEmbed()
         let args = message.content.split(' ').slice(1).join(' ');
     if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("*bc <message>");
-    return;
+        if (!args[1]) 
+		
     }
             message.guild.members.forEach(m => {
        if(!message.member.hasPermission('ADMINISTRATOR')) return;
